c1d1228fae7b612d885966cab27f85dd5ab63444
Yago <yago.lm@samsung.com>
2022-12-03 00:47:27 -0300

    finalizando put de articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -77,44 +77,56 @@ router.post("/admin/articles/delete", (req, res) =>{
     }
 })
 
-// router.get("/admin/categories/edit/:id", (req, res) =>{
+router.get("/admin/articles/edit/:id", (req, res) =>{
 
-//     var id = req.params.id
+    var id = req.params.id
 
-//     if(isNaN(id)){
-//         res.redirect("/admin/categories")
-//     }
+    if(isNaN(id)){
+        res.redirect("/admin/articles")
+    }
 
-//     Category.findByPk(id)
-//         .then(category => {
-//             if(category != undefined){
-//                 res.render("admin/categories/edit", {
-//                     category: category
-//                 })
-//             }else{
-//                 res.redirect("/admin/categories")
-//             }
-//         })
-//         .catch((error) => {
-//             console.error('Error: ', error);
-//         })
-// })
+    Article.findByPk(id)
+        .then(article => {
+            if(article != undefined){
 
-// router.post("/update", (req, res) => {
-//     var id = req.body.id
-//     var title = req.body.title
+                Category.findAll()
+                    .then((categories =>{
+                        res.render("admin/articles/edit", {
+                            categories: categories, article: article
+                        })
+                    })
+                )    
+            }else{
+                res.redirect("/")
+            }
+        })
+        .catch((error) => {
+            console.error('Error: ', error);
+        })
+})
 
-//     Category.update({ 
-//         title: title,
-//         slug: slugify(title)
-//     },{
-//         where: {
-//             id: id
-//         }
-//     }).then(() => {
-//         res.redirect("/admin/categories")
-//     })
-// })
+router.post("/update", (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update({ 
+        title: title,
+        body: body,
+        category: category,
+        slug: slugify(title)
+    },{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles")
+    }).catch((error) => {
+        console.error('Error: ', error);
+        res.redirect("/")
+    })
+})
 
 
 module.exports = router
