8e7d831d5b3d604df4144e25b393bda5111335d6
Yago <yago.lm@samsung.com>
2022-12-16 22:55:34 -0300

    add user in database wirth hash of password

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,43 +1,56 @@
 const express = require("express")
-const slugify = require("slugify")
-
 const router = express.Router()
+
+const bcrypt  = require("bcryptjs")
 
 
 const User = require("./User")
 
-router.get("/admin/users", (req, res) =>{
-
-    Category.findAll()
-        .then((categories =>{
-            res.render("admin/categories/index", {
-                categories: categories
-            })
-        })
-    )
-})
-
-router.get("/admin/categories/new", (req, res) =>{    
-    res.render("admin/categories/new")
+router.get("/admin/users/create", (req, res) =>{    
+    res.render("admin/users/create")
 })
 
 // router.post("/admin/categories/save", (req, res)=>{
-router.post("/save", (req, res) => { 
+router.post("/users/create", (req, res) => { 
 
-    var title = req.body.title;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({ 
+        where: {
+            email: email
+        }
+    }).then(user =>{
+        if(user == undefined){
+
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+        
+                User.create({
+                    email: email,
+                    password: hash
+                }).then(()=>{
+                    res.redirect("/admin/users")
+                }).catch(()=>{
+                    res.redirect("/")
+                })
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
+
     
-    if(title != undefined){
+})
 
-        Category.create({
-            title: title,
-            slug: slugify(title)
-        }).then(()=>{
-            res.redirect("/admin/categories")
+router.get("/admin/users", (req, res) =>{
+
+    User.findAll()
+        .then((users =>{
+            res.render("admin/users/index", {
+                users: users
+            })
         })
-
-    }else{
-        res.redirect("/admin/categories/new")
-    }
+    )
 })
 
 router.post("/delete", (req, res) =>{
